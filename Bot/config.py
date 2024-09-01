0237388d489cedef49d2eead94eb88f695ea499c
warjohn <47078498+warjohn@users.noreply.github.com>
2024-09-01 16:49:25 +0700

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,7 @@
 import concurrent.futures
 
 executor = concurrent.futures.ThreadPoolExecutor()
-token = "6941721145:AAEkIES8iOOJLw9pZ7O5LyFCg1Pn_PSJwLE"
+token = ""
 
 json_user = "data/user.json"
 json_QnA = "data/QnA.json"
